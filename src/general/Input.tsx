b4ddb955b932ce1d58d40011fd696d100da9d53f
Domeh John Kelvin <75913666+JhohannesK@users.noreply.github.com>
2023-05-30 03:26:58 +0000

    add password input (#14)

--- src/general/Input.tsx
+++ src/general/Input.tsx
@@ -1,13 +1,30 @@
-import { InputAdornment, SxProps, TextField } from '@mui/material'
+import { IconButton, InputAdornment, SxProps, TextField } from '@mui/material'
+import { useState } from 'react'
+import { MdOutlineVisibility, MdOutlineVisibilityOff } from 'react-icons/md'
 
 type GeneralInputType = {
   sx?: SxProps
   icon: React.ReactNode
   placeholder: string
   label: string
+  type?: 'text' | 'password'
 }
 
-const GeneralInput = ({ sx, icon, placeholder, label }: GeneralInputType) => {
+const GeneralInput = ({
+  sx,
+  icon,
+  placeholder,
+  label,
+  type,
+}: GeneralInputType) => {
+  const [showPassword, setShowPassword] = useState(false)
+  const handleClickShowPassword = () => setShowPassword((show) => !show)
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault()
+  }
   return (
     <>
       <TextField
@@ -15,11 +32,31 @@ const GeneralInput = ({ sx, icon, placeholder, label }: GeneralInputType) => {
         label={label}
         variant="outlined"
         placeholder={placeholder}
-        type="text"
+        type={
+          type === 'password' ? (showPassword ? 'text' : 'password') : 'text'
+        }
         sx={{ ...sx }}
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">{icon}</InputAdornment>
+          ),
+          endAdornment: (
+            <InputAdornment position="end">
+              {type === 'password' ? (
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? (
+                    <MdOutlineVisibilityOff />
+                  ) : (
+                    <MdOutlineVisibility />
+                  )}
+                </IconButton>
+              ) : null}
+            </InputAdornment>
           ),
         }}
         size="medium"
