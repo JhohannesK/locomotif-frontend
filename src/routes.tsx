beba0d8eeec1e245d1f0579ac3ae17bd9a620e88
maxxies <ahiamadzormaxwell7@gmail.com>
2023-06-01 17:18:26 +0000

    Add sigin page for institutions

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Constants from './utils/constants'
 import App from './App'
+import SiginPage from './pages/institution-signin-page/signin_page'
 
 const routhPaths = Constants.ROUTES
 
@@ -9,6 +10,10 @@ const routes = createBrowserRouter([
     path: routhPaths.root,
     element: <App />,
   },
+  {
+    path: routhPaths.institution_signin,
+    element:<SiginPage/>
+  }
 ])
 
 export default routes
