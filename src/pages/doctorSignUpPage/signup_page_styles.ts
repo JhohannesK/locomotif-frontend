c10f7e3da45c337af3612ef2a013fe408b9768ae
Nii <56045564+Barleymal-t@users.noreply.github.com>
2023-06-01 18:43:35 +0100

    Feature/doctor signup page (#20)
    * Build Sign Up Form
    
    * rename files

---
+++ src/pages/doctorSignUpPage/signup_page_styles.ts
@@ -0,0 +1,41 @@
+import { styled } from 'styled-components'
+import { colors } from '../../colors'
+
+export const SignInPageContainer = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const SignInPageContent = styled.section`
+  max-width: 300px;
+  width: 100%;
+  padding: 2rem;
+  border-radius: 1rem;
+`
+export const SignInPageTitle = styled.h1`
+  font-size: 2rem;
+  text-align: left;
+  margin-bottom: 2rem;
+`
+export const SignInPageFields = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  width: 100%;
+`
+export const SignInPageText = styled.div`
+  font-size: 0.7rem;
+`
+export const SignInPageButton = styled.div`
+  width: 100%;
+  border-radius: 0.5rem;
+  border-color: ${colors.button.pineGreen};
+  background-color: ${colors.button.pineGreen};
+  cursor: pointer;
+  border-style: none;
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+`
