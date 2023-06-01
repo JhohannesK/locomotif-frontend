e71207d4438b55d5ea2bdf4c0caefed4e69e8417
Kwabena Darkwa Obeng-Yeboah <darkwak@live.com>
2023-06-01 17:48:56 +0000

    Feature/landing page (#22)
    * :tada: Create practitioner and institution buttons
    
    * :tada: Create practitioner and institution buttons
    
    * unnecessary: checking out components
    
    * :lipstick: Add background color and make replace button with general button
    
    * :joy: finish first version of landing page with max width removed
    
    ---------
    
    Co-authored-by: jhohannesK <jdomeh77@gmail.com>

--- src/pages/landing-page/landingPage.tsx
+++ src/pages/landing-page/landingPage.tsx
@@ -1,31 +1,69 @@
-import Button from '@mui/material/Button'
 import { colors } from '../../colors'
-import { ContentWrapper, PageWrapper } from './styles'
+import {
+  ContentWrapper,
+  PageWrapper,
+  QuestionWrapper,
+  WelcomeWrapper,
+  SignedInWrapper,
+} from './styles'
+import GeneralButton from '../../general/Button'
 
 function LandingPage() {
   return (
     <PageWrapper>
       <ContentWrapper>
-        <h1>How do you like to get in?</h1>
+        <WelcomeWrapper>Welcome!</WelcomeWrapper>
+        <QuestionWrapper>Are you an Institution or a Doctor?</QuestionWrapper>
+        <GeneralButton
+          variantText={'contained'}
+          sx={{
+            backgroundColor: colors.button.aquamarine,
+            color: 'black',
+            height: '3rem',
+            width: '20rem',
+          }}
+          title="Practitioner"
+          size={'large'}
+        />
 
-        <Button
-          variant="contained"
-          sx={{ backgroundColor: colors.button.aquamarine, color: 'black' }}
-        >
-          Practioner
-        </Button>
-        <Button
+        <GeneralButton
           sx={{
             backgroundColor: colors.button.pineGreen,
             color: colors.button.aquamarine,
             borderColor: colors.button.aquamarine,
-            height: '7rem',
+            height: '3rem',
+            width: '20rem',
           }}
-          variant="outlined"
-          size="large"
-        >
-          Institution
-        </Button>
+          variantText={'outlined'}
+          title={'Institution'}
+          size={'small'}
+        />
+        <SignedInWrapper>
+          <GeneralButton
+            variantText={'contained'}
+            sx={{
+              backgroundColor: colors.button.aquamarine,
+              color: 'black',
+              height: '2rem',
+              width: '15rem',
+            }}
+            title="LOG iN"
+            size={'large'}
+          />
+
+          <GeneralButton
+            variantText={'outlined'}
+            sx={{
+              backgroundColor: colors.button.pineGreen,
+              color: colors.button.aquamarine,
+              borderColor: colors.button.aquamarine,
+              height: '2rem',
+              width: '15rem',
+            }}
+            title="sign iN"
+            size={'large'}
+          />
+        </SignedInWrapper>
       </ContentWrapper>
     </PageWrapper>
   )
--- src/pages/landing-page/styles.ts
+++ src/pages/landing-page/styles.ts
@@ -1,7 +1,10 @@
 import { styled } from 'styled-components'
+import { colors } from '../../colors'
 
-export const PageWrapper = styled.div`
+export const PageWrapper = styled.body`
   height: 100vh;
+  background-color: ${colors.background.pineGreen};
+  align-items: start;
 `
 
 export const ContentWrapper = styled.div`
@@ -11,4 +14,25 @@ export const ContentWrapper = styled.div`
   align-items: center;
   flex-direction: column;
   gap: 10px;
+`
+
+export const WelcomeWrapper = styled.h1`
+  font-family: 'Raleway';
+  font-weight: bold;
+  font-size: 7rem;
+  color: ${colors.text.whiteSmoke};
+`
+
+export const QuestionWrapper = styled.div`
+  font-family: 'Raleway';
+  font-size: 1.2rem;
+  color: ${colors.text.whiteSmoke};
+  text-align: center;
+`
+
+export const SignedInWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 25px;
+  padding-top: 5rem;
 `
