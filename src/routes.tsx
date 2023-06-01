02cbd723000107d69e37b721d542255a19faaa95
Maxwell Mawube <59958815+maxxies@users.noreply.github.com>
2023-06-01 17:24:26 +0000

    Use general input for password field and fix merge conflicts (#19)
    * Use general input for password field and fix merge conflicts
    
    * Change placeholder and route names

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Constants from './utils/constants'
 import App from './App'
+import SiginPageIns from './pages/institution-signin-page/signin_page'
 import SiginPage from './pages/doctor-signin-page/signin_page'
+<<<<<<< HEAD
 <<<<<<< HEAD
 import SiginPageIns from './pages/institution-signin-page/signin_page'
 =======
 import SiginPage from './pages/institution-signin-page/signin_page'
 >>>>>>> dba6536 (Add sigin page for institutions)
+=======
+>>>>>>> 502486d (Use general input for password field and fix merge conflicts (#19))
 
 const routhPaths = Constants.ROUTES
 
@@ -22,10 +26,6 @@ const routes = createBrowserRouter([
   {
     path: routhPaths.doctor_signin,
     element: <SiginPage />,
-  },
-  {
-    path: routhPaths.institution_signin,
-    element: <SiginPageIns />,
   },
 ])
 
