050531da4e086a181a45a957e7dbd2ad27fdc84a
maxxies <ahiamadzormaxwell7@gmail.com>
2023-06-01 17:50:36 +0000

    Modify routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,14 +3,6 @@ import Constants from './utils/constants'
 import App from './App'
 import SiginPageIns from './pages/institution-signin-page/signin_page'
 import SiginPage from './pages/doctor-signin-page/signin_page'
-<<<<<<< HEAD
-<<<<<<< HEAD
-import SiginPageIns from './pages/institution-signin-page/signin_page'
-=======
-import SiginPage from './pages/institution-signin-page/signin_page'
->>>>>>> dba6536 (Add sigin page for institutions)
-=======
->>>>>>> 502486d (Use general input for password field and fix merge conflicts (#19))
 
 const routhPaths = Constants.ROUTES
 
