c9602e89d9c341540f091f13d1ccdae3e367ad71
Nii <56045564+Barleymal-t@users.noreply.github.com>
2023-06-05 22:10:32 +0100

    Institution signup page (#37)
    * create signup form start
    
    * Combine authentication
    
    * Add Institution SignUp Route

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import Constants from './utils/constants'
 import App from './App'
 import SigninPageIns from './pages/Facility/facility-signin-page/signin_page'
+import SignUpPageIns from './pages/Facility/facility-signup-page/signup_page'
 import SigninPage from './pages/Personnel/personnel-signin-page/signin_page'
 import SignUpPage from './pages/Personnel/personnel-signup-page/signup_page'
 
@@ -15,6 +16,10 @@ const routes = createBrowserRouter([
   {
     path: routhPaths.facility_signin,
     element: <SigninPageIns />,
+  },
+  {
+    path: routhPaths.facility_signup,
+    element: <SignUpPageIns />,
   },
   {
     path: routhPaths.personnel_signin,
