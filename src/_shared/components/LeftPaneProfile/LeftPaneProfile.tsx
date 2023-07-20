94fcbd3430f4ac67aed69517a5fd586ff70be074
kwabendarkwa <darkwak@live.com>
2023-07-20 19:12:44 +0000

    :sparkles: Finish left pane profile

---
+++ src/_shared/components/LeftPaneProfile/LeftPaneProfile.tsx
@@ -0,0 +1,59 @@
+import { colors } from '../../../colors'
+import GeneralButton from '../../Button'
+import {
+  LeftPaneProfileContainer,
+  LeftProfileNameContainer,
+  LeftProfileWorkContainer,
+  LeftProfileWorkExperienceAndWork,
+  LeftProfileWorkExperienceContainer,
+  NavBarUserImageProfile,
+} from './styles'
+// import image from '../../../assets/user.jpeg'
+
+const LeftPaneProfile = ({
+  name,
+  jobTitle,
+  workExperience,
+}: LeftProfileProps) => {
+  return (
+    <LeftPaneProfileContainer>
+      <NavBarUserImageProfile>
+        {/* <img
+              src={image}
+              alt="health-leaf icon"
+              style={{ height: '100%', width: '100%', objectFit: 'cover' }}
+            /> */}
+      </NavBarUserImageProfile>
+      <LeftProfileWorkExperienceAndWork>
+        <LeftProfileNameContainer>{name}</LeftProfileNameContainer>
+        <LeftProfileWorkContainer>
+          {jobTitle} |{' '}
+          <LeftProfileWorkExperienceContainer>
+            {workExperience}
+          </LeftProfileWorkExperienceContainer>
+        </LeftProfileWorkContainer>
+      </LeftProfileWorkExperienceAndWork>
+
+      <GeneralButton
+        title="Edit Profile"
+        sx={{
+          backgroundColor: `${colors.button.pineGreen}`,
+          borderRadius: '10px',
+          color: `${colors.button.white}`,
+          textAlign: 'center',
+          fontFamily: 'Helvetica',
+          fontSize: '10px',
+          fontWeight: '400',
+          lineHeight: '137.14%',
+        }}
+      />
+    </LeftPaneProfileContainer>
+  )
+}
+export default LeftPaneProfile
+
+interface LeftProfileProps {
+  name: string
+  jobTitle: string
+  workExperience: string
+}
---
+++ src/_shared/components/LeftPaneProfile/styles.ts
@@ -0,0 +1,66 @@
+import { styled } from 'styled-components'
+import { colors } from '../../../colors'
+
+export const LeftPaneProfileContainer = styled.div`
+  width: 296px;
+  height: 304px;
+  border-radius: 10px;
+  border: 1px solid rgba(209, 209, 209, 0.5);
+  background: #fff;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-evenly;
+  padding: 10px 0px;
+`
+
+export const LeftProfileButtonContainer = styled.div``
+
+export const LeftProfileNameContainer = styled.div`
+  color: #000000;
+  font-family: Helvetica;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 137.14%;
+`
+
+export const LeftProfileWorkContainer = styled.div`
+  font-family: Helvetica;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 137.14%;
+  color: rgba(0, 0, 0, 0.6);
+`
+
+export const LeftProfileWorkExperienceContainer = styled.span`
+  font-family: Helvetica;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 137.14%;
+  color: rgba(0, 0, 0, 0.8);
+`
+
+export const LeftProfileWorkExperienceAndWork = styled.div`
+gap: 5px
+display: flex;
+flex-direction: column; 
+align-items: center;
+justify-content: center;
+text-align: center;
+`
+
+export const NavBarUserImageProfile = styled.div`
+  outline: 1px solid orange;
+  height: 98px;
+  width: 98px;
+  clip-path: circle();
+  background-color: ${colors.background.whiteSmoke};
+  margin-right: 10px;
+  background-size: cover;
+  background-position: center center;
+`
+
+export const LeftProfileImageContainer = styled.div``
