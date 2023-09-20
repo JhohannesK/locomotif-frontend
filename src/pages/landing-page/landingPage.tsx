17216311aa0ee5f68523c53b9184d748b287561d
kwabendarkwa <darkwak@live.com>
2023-09-20 17:56:41 +0000

    :sparkles: Finish first of the new version of the dashboard

--- src/pages/landing-page/landingPage.tsx
+++ src/pages/landing-page/landingPage.tsx
@@ -1,163 +1,53 @@
-import { colors } from '../../colors'
 import {
-  ContentWrapper,
-  PageWrapper,
-  QuestionWrapper,
-  WelcomeWrapper,
-  SignedInWrapper,
-  UserWrapper,
-  ButtonWrapper,
+  BottomContent,
+  Content,
+  Facility,
+  GetStarted,
+  JoinLocomotif,
+  Logo,
+  MainContent,
+  Page,
+  Personnel,
+  TopContent,
 } from './styles'
-import GeneralButton from '../../_shared/components/button/Button'
-import { useState } from 'react'
-import Constants from '../../utils/constants'
-import routes from '../../routes'
+import facility from '../../_shared/assets/facility.jpeg'
+import doctor from '../../_shared/assets/doctor.jpeg'
 
 function LandingPage() {
-  const [isPersonnelClickedButton, setIsPersonnelClickedButton] =
-    useState(false)
-  const [isFacilityClickedButton, setIsFacilityClickedButton] = useState(false)
-
   return (
-    <PageWrapper>
-      <ContentWrapper>
-        <WelcomeWrapper>Welcome!</WelcomeWrapper>
-        <QuestionWrapper>Are you a Facility or a Practitioner?</QuestionWrapper>
-
-        <ButtonWrapper>
-          <UserWrapper>
-            <GeneralButton
-              // variantText={'contained'}
-              sx={{
-                backgroundColor:
-                  isPersonnelClickedButton === true
-                    ? colors.button.white
-                    : colors.button.aquamarine,
-                color: colors.button.pineGreen,
-                height: isPersonnelClickedButton === true ? '4rem' : '3rem',
-                width: isPersonnelClickedButton === true ? '23rem' : '20rem',
-                textTransform: 'uppercase',
-                ':hover': {
-                  background: colors.button.aquamarine,
-                },
-                // borderColor: `${isFacilityClickedButton?colors.button.pineGreen:colors.button.aquamarine}`
-              }}
-              title="Practitioner"
-              size={'large'}
-              onClick={() => {
-                setIsPersonnelClickedButton(true)
-                setIsFacilityClickedButton(false)
-              }}
-            />
-            <GeneralButton
-              sx={{
-                backgroundColor:
-                  isFacilityClickedButton === true
-                    ? colors.button.white
-                    : colors.button.pineGreen,
-                color:
-                  isFacilityClickedButton === true
-                    ? colors.button.aquamarine
-                    : colors.button.aquamarine,
-                borderColor:
-                  isFacilityClickedButton === true
-                    ? colors.button.pineGreen
-                    : colors.button.aquamarine,
-                height: isFacilityClickedButton === true ? '4rem' : '3rem',
-                width: isFacilityClickedButton === true ? '23rem' : '20rem',
-                textTransform: 'uppercase',
-                ':hover': {
-                  borderColor: colors.button.aquamarine,
-                },
-              }}
-              variantText={'outlined'}
-              title={'Facility'}
-              size={'small'}
-              onClick={() => {
-                setIsFacilityClickedButton(true)
-                setIsPersonnelClickedButton(false)
-              }}
-            />
-          </UserWrapper>
-          {isPersonnelClickedButton && (
-            <SignedInWrapper>
-              <GeneralButton
-                variantText={'contained'}
-                sx={{
-                  backgroundColor: colors.button.aquamarine,
-                  color: colors.button.pineGreen,
-                  height: '2rem',
-                  width: '9rem',
-                  textTransform: 'uppercase',
-                }}
-                title="LOG IN"
-                size={'large'}
-                onClick={() => {
-                  routes.navigate(Constants.ROUTES.personnel_signin)
+    <Page>
+      <Content>
+        <Logo>Loco</Logo>
+        <MainContent>
+          <TopContent>
+            <GetStarted>Get Started With Locomotif</GetStarted>
+            <JoinLocomotif>
+              Join Locomotif as a Personnel or Facility
+            </JoinLocomotif>
+          </TopContent>
+          <BottomContent>
+            <Facility>
+              <img src={facility} alt="facility" />
+              <div>Join As a health Care Facility</div>
+            </Facility>
+            <Personnel>
+              <img
+                src={doctor}
+                alt="facility"
+                style={{
+                  height: '500px',
+                  width: '100%',
+                  maxWidth: '400px',
+                  objectFit: 'cover',
+                  borderRadius: '12px',
                 }}
               />
-              <GeneralButton
-                variantText={'outlined'}
-                sx={{
-                  backgroundColor: colors.button.pineGreen,
-                  color: colors.button.aquamarine,
-                  borderColor: colors.button.aquamarine,
-                  height: '2rem',
-                  width: '9rem',
-                  ':hover': {
-                    borderColor: colors.button.aquamarine,
-                  },
-                  textTransform: 'uppercase',
-                }}
-                title="SIGN UP"
-                size={'large'}
-                onClick={() => {
-                  routes.navigate(Constants.ROUTES.personnel_signup)
-                }}
-              />
-            </SignedInWrapper>
-          )}
-          {isFacilityClickedButton && (
-            <SignedInWrapper>
-              <GeneralButton
-                variantText={'contained'}
-                sx={{
-                  backgroundColor: colors.button.aquamarine,
-                  color: colors.button.pineGreen,
-                  height: '2rem',
-                  width: '9rem',
-                  textTransform: 'uppercase',
-                }}
-                title="LOG IN"
-                size={'large'}
-                onClick={() => {
-                  routes.navigate(Constants.ROUTES.facility_signin)
-                }}
-              />
-              <GeneralButton
-                variantText={'outlined'}
-                sx={{
-                  backgroundColor: colors.button.pineGreen,
-                  color: colors.button.aquamarine,
-                  borderColor: colors.button.aquamarine,
-                  height: '2rem',
-                  width: '9rem',
-                  ':hover': {
-                    borderColor: colors.button.aquamarine,
-                  },
-                  textTransform: 'uppercase',
-                }}
-                title="SIGN UP"
-                size={'large'}
-                onClick={() => {
-                  routes.navigate(Constants.ROUTES.facility_signup)
-                }}
-              />
-            </SignedInWrapper>
-          )}
-        </ButtonWrapper>
-      </ContentWrapper>
-    </PageWrapper>
+              <div>Register As a Personnel</div>
+            </Personnel>
+          </BottomContent>
+        </MainContent>
+      </Content>
+    </Page>
   )
 }
 
--- src/pages/landing-page/styles.ts
+++ src/pages/landing-page/styles.ts
@@ -1,53 +1,149 @@
 import { styled } from 'styled-components'
 import { colors } from '../../colors'
+import Constants from '../../utils/constants'
 
-export const PageWrapper = styled.body`
+//remember to change the size of the fonts on bigger screens
+export const Page = styled.body`
   height: 100vh;
-  background-color: ${colors.background.pineGreen};
-  align-items: start;
-  background-color: ${colors.background.pineGreen};
-  align-items: start;
-`
-
-export const ContentWrapper = styled.div`
-  display: flex;
-  height: 100%;
-  justify-content: center;
+  width: 100vw;
+  background-color: ${colors.background.white};
   align-items: center;
+  justify-content: center;
+  display: flex;
+`
+export const Content = styled.div`
+  display: flex;
   flex-direction: column;
-  gap: 10px;
+  align-items: center;
+  gap: 13px;
+  width: 95%;
+  height: 70%;
+  max-width: 836px;
 `
 
-export const WelcomeWrapper = styled.h1`
-  font-weight: bold;
-  font-size: 7rem;
-  color: ${colors.text.platinum};
-  @media (max-width: 768px) {
-    font-size: 4rem;
+export const Logo = styled.div`
+  top: 0;
+  color: var(--Primary-Color, #0e6655);
+  font-family: Helvetica;
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`
+export const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+  width: 100%;
+  flex-grow: 1;
+`
+
+export const TopContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0px;
+  width: 100%;
+  height: 20%;
+`
+export const BottomContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  height: 100%;
+  align-items: center;
+  width: 100%;
+  border-radius: 12px;
+  background-color: brown;
+  ${Constants.LAYOUT.MEDIA_QUERIES.TABLET_480_PX} {
+    gap: 30px;
   }
 `
 
-export const QuestionWrapper = styled.div`
-  font-size: 1.2rem;
-  color: ${colors.text.platinum};
-  text-align: center;
+export const GetStarted = styled.div`
+  color: #000;
+  font-feature-settings: 'clig' off, 'liga' off;
+  font-family: Helvetica;
+  font-size: 1.7em;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 137.14%; /* 68.57px */
+  ${Constants.LAYOUT.MEDIA_QUERIES.TABLET_480_PX} {
+    font-size: 2.5em;
+  }
+  ${Constants.LAYOUT.MEDIA_QUERIES.TABLET_PX} {
+    font-size: 50px;
+  }
+`
+export const JoinLocomotif = styled.div`
+  color: var(--Tertiary, rgba(0, 0, 0, 0.7));
+  font-family: Helvetica;
+  font-size: 1em;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  ${Constants.LAYOUT.MEDIA_QUERIES.TABLET_480_PX} {
+    font-size: 2.5em;
+  }
+  ${Constants.LAYOUT.MEDIA_QUERIES.TABLET_PX} {
+    font-size: 25px;
+  }
 `
 
-export const SignedInWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  padding-top: 5rem;
-  justify-content: space-between;
-`
-export const UserWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  justify-content: center;
-  align-items: center;
+export const Facility = styled.div`
+  background-color: blue;
+  position: relative; /* Ensure relative positioning for containing absolute positioned text */
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    object-fit: cover;
+    border-radius: 12px;
+    height: 500px;
+  }
+
+  div {
+    position: absolute; /* Position text absolutely within the container */
+    bottom: 36px;
+    left: 0;
+    right: 0; /* Center the text horizontally */
+    text-align: center; /* Center the text horizontally within the container */
+    color: #fff;
+    /* Main Header */
+    font-family: Helvetica;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  }
 `
 
-export const ButtonWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+export const Personnel = styled.div`
+  background-color: green;
+  position: relative; /* Ensure relative positioning for containing absolute positioned text */
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    object-fit: cover;
+    border-radius: 12px;
+  }
+
+  /* Center the text vertically and horizontally within the container */
+  div {
+    position: absolute; /* Position text absolutely within the container */
+    bottom: 36px;
+    left: 0;
+    right: 0; /* Center the text horizontally */
+    text-align: center; /* Center the text horizontally within the container */
+    color: #fff;
+    /* Main Header */
+    font-family: Helvetica;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  }
 `
