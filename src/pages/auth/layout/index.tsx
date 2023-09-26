140c0ddb8fd02e79379407b83d0c43aa7d34658e
jhohannesK <jdomeh77@gmail.com>
2023-09-26 23:03:17 +0000

    style the sidebar

--- src/pages/auth/layout/index.tsx
+++ src/pages/auth/layout/index.tsx
@@ -4,19 +4,53 @@ import styled from 'styled-components'
 import { colors } from '../../../colors'
 
 const PersonnelSteps = [
-  'Personnel/Facility ',
-  'Create an account',
-  'Terms and Conditions',
-  'Create Profile',
-  'Upload your CV',
-  'Completed',
+  {
+    title: 'Personnel or Facility',
+    subTitle: 'Join us as a Personnel or Facility',
+  },
+  {
+    title: 'Create an account',
+    subTitle: 'Create an account to get started',
+  },
+  {
+    title: 'Terms and Conditions',
+    subTitle: 'Read and accept our terms and conditions',
+  },
+  {
+    title: 'Create Profile',
+    subTitle: 'Provide us with your details',
+  },
+  {
+    title: 'Upload your CV',
+    subTitle: 'Upload your CV and other documents',
+  },
+  {
+    title: 'Completed',
+    subTitle: 'You are all set',
+  },
 ]
 
 const FacilitySteps = [
-  'Facility',
-  'Create an account',
-  'Terms and Conditions',
-  'Completed',
+  {
+    title: 'Personnel or Facility',
+    subTitle: 'Join us as a Personnel or Facility',
+  },
+  {
+    title: 'Create an account',
+    subTitle: 'Create an account to get started',
+  },
+  {
+    title: 'Terms and Conditions',
+    subTitle: 'Read and accept our terms and conditions',
+  },
+  {
+    title: 'Create Profile',
+    subTitle: 'Setup your facility profile page',
+  },
+  {
+    title: 'Completed',
+    subTitle: 'You are all set',
+  },
 ]
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
