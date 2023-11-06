0b433eea82e7e016ed2c12c7d0fac2098bc19f1c
Kwabena Darkwa Obeng-Yeboah <darkwak@live.com>
2023-11-06 10:26:11 +0000

    :sparkles: Add ui for the first part of the filtering component (#185)

--- src/pages/Personnel/personnel-applications-page/applicationsPage/applicationsPage.tsx
+++ src/pages/Personnel/personnel-applications-page/applicationsPage/applicationsPage.tsx
@@ -4,10 +4,15 @@ import LeftPane from '../../personnel-home-page/components/LeftPane'
 import ApplicationsCard, {
   ApplicationsCardProps,
 } from '../applicationsCard/applicationsCard'
-import { FilterPane, RightPaneContainer, Wrapper } from './styles'
+import {
+  RightPaneContainer,
+  Wrapper,
+  LeftPaneContainer,
+  ApplicationCardsContainer,
+} from './styles'
 import applicationsData from '../../../mocks/applications.json'
 import Grid from '@mui/material/Grid'
-import { LeftPaneContainer } from '../../personnel-home-page/styles'
+import FilterApplications from '../filterApplicationCards/filterApplicationCards'
 
 const ApplicationsPage = () => {
   const applicationsCardDetails: ApplicationsCardProps[] =
@@ -19,14 +24,22 @@ const ApplicationsPage = () => {
           <LeftPane />
         </LeftPaneContainer>
         <RightPaneContainer>
-          <FilterPane></FilterPane>
-          <Grid container spacing={5}>
-            {applicationsCardDetails?.map((application, index) => (
-              <Grid item xs={12} sm={6} md={4} key={index}>
-                <ApplicationsCard application={application} />
-              </Grid>
-            ))}
-          </Grid>
+          <FilterApplications></FilterApplications>
+          <ApplicationCardsContainer>
+            <Grid
+              container
+              spacing={2}
+              justifyContent="center"
+              alignItems="center"
+              columnSpacing={{ xs: 1, sm: 4, md: 9 }}
+            >
+              {applicationsCardDetails?.map((application, index) => (
+                <Grid item xs={12} sm={6} md={4} key={index}>
+                  <ApplicationsCard application={application} />
+                </Grid>
+              ))}
+            </Grid>
+          </ApplicationCardsContainer>
         </RightPaneContainer>
       </Wrapper>
     </Layout>
