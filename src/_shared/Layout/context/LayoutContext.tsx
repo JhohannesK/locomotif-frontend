07b6f3e31e80094a1d2ac6fecf2511a3e4216e5b
jhohannesK <jdomeh77@gmail.com>
2023-12-04 11:07:39 +0000

    fix issues with dialog

--- src/_shared/Layout/context/LayoutContext.tsx
+++ src/_shared/Layout/context/LayoutContext.tsx
@@ -6,6 +6,9 @@ interface ILayoutContext {
   handleClose: () => void
   handleOpen: () => void
   isSuccess: boolean
+  setSuccess: React.Dispatch<React.SetStateAction<boolean>>
+  isDanger?: boolean
+  setDanger: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 export const LayoutContext = createContext<ILayoutContext>({
@@ -13,15 +16,28 @@ export const LayoutContext = createContext<ILayoutContext>({
   setOpen: () => {},
   handleClose: () => {},
   handleOpen: () => {},
+  setSuccess: () => {},
+  setDanger: () => {},
   isSuccess: false,
+  isDanger: false,
 })
 
 const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
   const [open, setOpen] = React.useState(false)
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
-  const isSuccess = false
-  const value = { open, setOpen, handleClose, handleOpen, isSuccess }
+  const [isSuccess, setSuccess] = React.useState(false)
+  const [isDanger, setDanger] = React.useState(false)
+  const value = {
+    open,
+    setOpen,
+    handleClose,
+    handleOpen,
+    isSuccess,
+    isDanger,
+    setSuccess,
+    setDanger,
+  }
   return (
     <LayoutContext.Provider value={value}>{children} </LayoutContext.Provider>
   )
--- src/pages/Personnel/posting/hook/usePosting.tsx
+++ src/pages/Personnel/posting/hook/usePosting.tsx
@@ -1,12 +1,16 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import Constants from '../../../../utils/constants'
 import { loadFromLocalStorage } from '../../../../redux/hooks/middleware'
+import { useContext } from 'react'
+import { LayoutContext } from '../../../../_shared/Layout/context/LayoutContext'
 
 const usePosting = () => {
   const postingId = loadFromLocalStorage({
     key: Constants.LOCALSTORAGE_KEYS.POSTINGID,
   })
+  const { setDanger } = useContext(LayoutContext)
+
   const getPostingByPostingId = async (postingId: number) => {
     const response = await axios.get(
       `${Constants.BaseURL}postings/${postingId}`
@@ -15,14 +19,33 @@ const usePosting = () => {
     return data
   }
 
+  const applyForPosting = async (postingId: number) => {
+    const response = await axios.post(
+      `${Constants.BaseURL}postings/applications/`,
+      { posting: postingId }
+    )
+    const data = await response.data
+    return data
+  }
+
   const { data, isLoading } = useQuery({
-    queryKey: ['posting', postingId],
+    queryKey: ['getPosting', postingId],
     queryFn: () => getPostingByPostingId(postingId),
     enabled: postingId !== undefined,
   })
+
+  const { mutate } = useMutation({
+    mutationKey: ['createPosting', postingId],
+    mutationFn: () => applyForPosting(postingId),
+    onError: () => {
+      setDanger(true)
+    },
+  })
+
   return {
     data,
     isLoading,
+    mutate,
   }
 }
 
