c3f471696043d16f7a15ea583ee43cf4a57798c1
jhohannesK <jdomeh77@gmail.com>
2024-02-13 17:11:30 +0000

    change slice

--- src/redux/slices/appSlice.ts
+++ src/redux/slices/appSlice.ts
@@ -5,7 +5,7 @@ import { TJobSteps } from '../../types'
 
 const initialState: AppState = {
   activeSidebar: 1,
-  activeJobPublishingStep: TJobSteps['Pay type'],
+  activeJobPublishingStep: TJobSteps['Job Details'],
 }
 
 export const appSlice = createSlice({
